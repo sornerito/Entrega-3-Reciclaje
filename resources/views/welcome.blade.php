59732be062e9ae544630e70e8e0cb75e44ce8ca2
sornerito <samueleonardo.escuela@gmail.com>
2025-08-13 23:50:30 -0500

    desastrozo primer commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HOLAAA</title>
+</head>
+<body>
+    <p>
+Hola este es un texto de exito, y ya puedo mostrar vainas
+<br><br>
+miren esta portada durisima de jazz
+</p>
+    
+    <br>
+    <img width="300" src="https://cdn.cosmos.so/5e7063bf-419d-4bd8-a45e-25b3225a9ec6?format=jpeg" alt="">
+</body>
+</html>
